c73a6f28531f25f81003c74e3a442630e054c592
anhonestmouse <anhonestmouse@gmail.com>
2017-12-04 18:17:47 -0700

    makes it better thru less code lines

--- myReduce.js
+++ myReduce.js
@@ -4,20 +4,30 @@
 function myReduce(arr, callback) {
 
 //  CODE INSIDE HERE   //
-	let x =arr[0];
-	if(arguments.length > 2){
-		x = arguments[2];
-		for(let i = 0; i<arr.length;++i){
-			x = callback(x,arr[i],i,arr);
-		}
-	} else {
-		for(let i = 1; i<arr.length;++i){
-			x = callback(x,arr[i],i,arr);
-		}
-	}
-	return x;
+// 	let x =arr[0];
+// 	if(arguments.length > 2){
+// 		x = arguments[2];
+// 		for(let i = 0; i<arr.length;++i){
+// 			x = callback(x,arr[i],i,arr);
+// 		}
+// 	} else {
+// 		for(let i = 1; i<arr.length;++i){
+// 			x = callback(x,arr[i],i,arr);
+// 		}
+// 	}
+// 	return x;
+// }
+let x = arr[0]
+let i = 1;
+if(arguments.length>2){
+	x=arguments[2];
+	i=0;
 }
-
+for(i;i<arr.length;++i){
+	x = callback(x,arr[i],i,arr);
+}
+return x;
+}
 /*
  Best if you don't code out here.
  If you want to check your code, use `index.js`!
